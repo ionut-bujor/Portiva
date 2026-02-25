9eb9508c8f059a9d4d6b64c30892c888d4da51e5
Bujor Ionut (2024) ZNAC600 <ionut.bujor.2024@live.rhul.ac.uk>
2026-02-25 11:30:33 +0000

    Initialised Spring

---
+++ Backend/src/main/java/portiva/backend/PortivaApplication.java
@@ -0,0 +1,13 @@
+package portiva.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PortivaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PortivaApplication.class, args);
+	}
+
+}
---
+++ Backend/src/test/java/portiva/backend/PortivaApplicationTests.java
@@ -0,0 +1,13 @@
+package portiva.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PortivaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
