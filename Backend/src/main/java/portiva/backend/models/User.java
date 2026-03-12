f71603ba2b60b8035dd1f78c1baf18e8e62bf049
Bujor Ionut (2024) ZNAC600 <ionut.bujor.2024@live.rhul.ac.uk>
2026-03-12 12:44:31 +0000

    #2 changed the table name as user is restricted

--- Backend/src/main/java/portiva/backend/models/User.java
+++ Backend/src/main/java/portiva/backend/models/User.java
@@ -7,8 +7,10 @@ import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.OneToOne;
+import jakarta.persistence.Table;
 
 @Entity
+@Table(name = "users")
 public class User {
   @Id
   @GeneratedValue(strategy = GenerationType.IDENTITY)
